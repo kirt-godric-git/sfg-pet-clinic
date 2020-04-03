5eff340710313a6d59f3317b1b823ddd4ea6dc90
Kirt Godric <cbk_kirtkyamko@yahoo.com>
2020-04-03 19:51:45 +0300

    upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/AppTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/AppTest.java
@@ -1,36 +1,47 @@
 package guru.springframework.sfgpetclinic;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+//**** JUnit 3 & 4 API imports ****
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+//**** JUnit 5 API imports ****
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+//public class AppTest extends TestCase	// <-- used by JUnit 3 & 4
+@ExtendWith(SpringExtension.class)	// <-- used by JUnit 5
+public class AppTest 	
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        //super( testName );	// <-- used by JUnit 3 & 4 on extends TestCase
+//    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+// ******* used by JUnit 3 & 4 ************
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
+    @Test	// <-- used by both JUnit 4 & 5
     public void testApp()
     {
         assertTrue( true );
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,15 +1,24 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// **** JUnit 4 API imports ****
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+//**** JUnit 5 API imports ****
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+//@RunWith(SpringRunner.class)		// <-- used by JUnit 4
+@ExtendWith(SpringExtension.class)	// <-- used by JUnit 5
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
-	@Test
+	@Test	// <-- used by both JUnit 4 & 5
 	public void contextLoads() {
 	}
 
