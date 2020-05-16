51eb8a5c4f5af07ce08d7257a8c65876e8a3797e
Kirt Godric <cbk_kirtkyamko@yahoo.com>
2020-05-16 22:32:03 +0300

    Added create update owner. Closes #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -28,4 +28,8 @@ public class BaseEntity implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}*/
+	
+	public boolean isNew() {
+		return this.id == null;
+	}
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +26,8 @@ import guru.springframework.sfgpetclinic.services.VisitService;
 public class OwnerController {
 	private final OwnerService ownerService;
 	private final VisitService visitService;
+	
+	private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
 	
 	@Autowired	// <-- optional to include this
 	public OwnerController(OwnerService ownerService, VisitService visitService) {
@@ -93,5 +98,42 @@ public class OwnerController {
 		}*/
 		mav.addObject(owner);
 		return mav;
-	}	
+	}
+	
+	@GetMapping("/new")
+	public String initCreationForm(Model model) {
+		//model.addAttribute("owner", new Owner()); 			// OK - Using traditional
+		model.addAttribute("owner", Owner.builder().build());	// OK - Using Lombok
+		return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+	}
+
+	@PostMapping("/new")
+	public String processCreationForm(@Valid Owner owner, BindingResult result) {
+		if (result.hasErrors()) {
+			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			Owner savedOwner = ownerService.save(owner);
+			return "redirect:/owners/" + savedOwner.getId();
+		}
+	}
+	
+	@GetMapping("/{ownerId}/edit")
+	public String initUpdateOwnerForm(@PathVariable("ownerId") Long ownerId, Model model) {
+		model.addAttribute(ownerService.findById(ownerId));
+		return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+	}
+
+	@PostMapping("/{ownerId}/edit")
+	public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result, @PathVariable Long ownerId) {
+		if (result.hasErrors()) {
+			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			owner.setId(ownerId);
+			Owner savedOwner = ownerService.save(owner);
+			//return "redirect:/owners/{ownerId}";
+			return "redirect:/owners/"+savedOwner.getId();
+		}
+	}
 }
