d0cde014d1ef33237f9e31a49149bb7ae51eb4d6
Kirt Godric <cbk_kirtkyamko@yahoo.com>
2020-05-16 20:24:07 +0300

    Updated

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -2,10 +2,21 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import guru.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 	Owner findByLastName(String lastName); // <-- This will use Dynamic Queries of SpringDataJPA
+	
+	// *** Version from Spring Pet Clinic --> OK working also
+	/*@Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName%")
+	@Transactional(readOnly = true)
+	List<Owner> findAllByLastNameLike(@Param("lastName") String lastName);*/
+	
+	// *** Version from Spring Guru speaker
 	List<Owner> findAllByLastNameLike(String lastName);
 }
